004f61fd4c2ed7046430bb8ba34a490dcd18cd38
nsingh-93 <neils@vt.edu>
2019-08-07 15:34:23 -0400

    Started working on rps game
    Set up html file and getting functions names out  so far

---
+++ rock-paper-scissors.js
@@ -0,0 +1,12 @@
+function playerPlay() {}
+
+function computerPlay() {}
+
+function playRound(playerSelection, computerSelection) {}
+
+function game() {
+    let playerScore = computerScore = 0;
+    for (round = 0; round < 5; round++) {}
+}
+
+// game();
